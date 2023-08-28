c17a33608e8b6b9d797830b820fd0cbc882fdbd6
Martin Nikov <mnikov12@gmail.com>
2023-08-28 12:17:05 +0300

    update(packages/nimbus-eth2): Update `nimbus-eth2` to v23.8.0

--- packages/clients/consensus/nimbus-eth2/default.nix
+++ packages/clients/consensus/nimbus-eth2/default.nix
@@ -6,7 +6,6 @@
   nim,
   cmake,
   which,
-  fetchpatch,
   writeScriptBin,
   # Options: nimbus_light_client, nimbus_validator_client, nimbus_signing_node
   makeTargets ? ["all"],
@@ -24,25 +23,16 @@
 assert nim.version == "1.6.12" || nim.version == "1.6.14";
   stdenv.mkDerivation rec {
     pname = "nimbus";
-    rev = "187e1a06335e36bbc508fff38729833d154edbaa";
-    version = "23.6.1";
+    rev = "d014d0a51cca1dda13d34e3de860aa4287afdb36";
+    version = "23.8.0";
 
     src = fetchFromGitHub {
       owner = "status-im";
       repo = "nimbus-eth2";
       inherit rev;
-      hash = "sha256-O7jxRuJZkrdNZVZ3jMOPjTh/tWk3XgPwx5A0xgELvAU=";
+      hash = "sha256-NfCXooS3vCrLzKYusl4MeMSVtc/ZQzRnq2E+QJUhNL8=";
       fetchSubmodules = true;
     };
-
-    patches = [
-      # Nim 1.6.14 support
-      (fetchpatch {
-        name = "nim-v1.6.14-support.patch";
-        url = "https://github.com/status-im/nimbus-eth2/commit/41b93ae57a7bb32758f453a09259ae44b37e3db9.patch";
-        hash = "sha256-yvXREhqc/C0Yo2ioTaOFFw7KAc0WgDkoM5SXXnaIjrQ=";
-      })
-    ];
 
     # Fix for Nim compiler calling 'git rev-parse' and 'lsb_release'.
     nativeBuildInputs = let
