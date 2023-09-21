c2f0f9b6ea4f98cf0c4713652dcea9c125d0c3a8
Martin Nikov <mnikov12@gmail.com>
2023-09-21 12:52:30 +0300

    update(packages/nimbus-eth2): Update `nimbus-eth2` to v23.9.0

--- packages/clients/consensus/nimbus-eth2/default.nix
+++ packages/clients/consensus/nimbus-eth2/default.nix
@@ -23,14 +23,14 @@
 assert nim.version == "1.6.12" || nim.version == "1.6.14";
   stdenv.mkDerivation rec {
     pname = "nimbus";
-    rev = "d014d0a51cca1dda13d34e3de860aa4287afdb36";
-    version = "23.8.0";
+    rev = "568e1fbb8cd401af55a405687a34cca2ff0ff883";
+    version = "23.9.0";
 
     src = fetchFromGitHub {
       owner = "status-im";
       repo = "nimbus-eth2";
       inherit rev;
-      hash = "sha256-NfCXooS3vCrLzKYusl4MeMSVtc/ZQzRnq2E+QJUhNL8=";
+      hash = "sha256-aTuFyaOnXhfBbOvbDth4ECIrdekVdz/eEMcHXOxzX5w=";
       fetchSubmodules = true;
     };
 
