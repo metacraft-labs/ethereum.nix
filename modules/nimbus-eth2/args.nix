7eabb3f1d60239e30b6966b5de85087f423efeec
Martin Nikov <mnikov12@gmail.com>
2023-11-29 12:33:39 +0200

    feat(modules): Add `holesky` network to `nimbus-eth` and `mev-boost`

--- modules/nimbus-eth2/args.nix
+++ modules/nimbus-eth2/args.nix
@@ -1,7 +1,7 @@
 lib:
 with lib; {
   network = mkOption {
-    type = types.nullOr (types.enum ["goerli" "prater" "ropsten" "sepolia"]);
+    type = types.nullOr (types.enum ["goerli" "prater" "ropsten" "sepolia" "holesky"]);
     default = null;
     description = mdDoc "The network to connect to. Mainnet (null) is the default ethereum network.";
   };
