698ff59b15f7ec915dedb460378e9598de4f1a60
Petar Kirov <petar.p.kirov@gmail.com>
2024-10-20 20:36:43 +0300

    blutgang: 0.3.1 -> 0.3.6

--- pkgs/blutgang/default.nix
+++ pkgs/blutgang/default.nix
@@ -1,34 +1,44 @@
 {
+  lib,
   fetchFromGitHub,
   openssl,
   pkg-config,
   rustPlatform,
+  rust-jemalloc-sys,
+  stdenv,
+  darwin,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "blutgang";
-  version = "0.3.1";
+  version = "0.3.6";
 
   src = fetchFromGitHub {
     owner = "rainshowerLabs";
     repo = pname;
     rev = "Blutgang-${version}";
-    hash = "sha256-prJq1enn2bJdJieVjvq1vd7dCNBlg5ppymIwjU4pgzg=";
+    hash = "sha256-EAmmCvESMneYuoTEa8Qm5eYqJkkRDY8CqlfsER1Pq8s=";
   };
+
+  cargoHash = "sha256-1G80j/lZrAlrgOLgpKyGYP9x6g/9kxXf3wmY2OcynFc=";
 
   nativeBuildInputs = [
     pkg-config
   ];
 
-  buildInputs = [
-    openssl
-  ];
-
-  cargoHash = "sha256-bAHUcfRtefGsRgFMBY5JJ4QSstB8wApcdqz/pqSVpuk=";
+  buildInputs =
+    [openssl rust-jemalloc-sys]
+    ++ lib.optionals stdenv.isDarwin [
+      darwin.apple_sdk.frameworks.Security
+      darwin.apple_sdk.frameworks.SystemConfiguration
+    ];
 
   meta = {
-    description = "the wd40 of ethereum load balancers";
+    description = "The wd40 of ethereum load balancers";
     homepage = "https://github.com/rainshowerLabs/blutgang";
+    # Functional Source License, Version 1.0, Apache 2.0 Change License
+    # https://github.com/rainshowerLabs/blutgang/blob/Blutgang-0.3.6/LICENSE
+    license = lib.licenses.unfree;
     mainProgram = "blutgang";
-    platforms = ["x86_64-linux"];
+    platforms = ["x86_64-linux" "aarch64-darwin"];
   };
 }
