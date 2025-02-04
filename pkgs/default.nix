ed366e91debff15bb67aad928fc27f86ed92230a
Galin Stefanov <galinstef10@gmail.com>
2025-02-04 13:51:22 +0200

    style(pkgs/default.nix): Format file

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,173 +3,184 @@
   inputs,
   lib,
   ...
-}: {
+}:
+{
   # add all our packages based on host platform
-  flake.overlays.default = _final: prev: let
-    inherit (prev.stdenv.hostPlatform) system;
-  in
-    if builtins.hasAttr system self.packages
-    then self.packages.${system}
-    else {};
+  flake.overlays.default =
+    _final: prev:
+    let
+      inherit (prev.stdenv.hostPlatform) system;
+    in
+    if builtins.hasAttr system self.packages then self.packages.${system} else { };
 
-  perSystem = {
-    self',
-    inputs',
-    pkgs,
-    pkgsUnstable,
-    pkgs2311,
-    pkgsNethermind,
-    system,
-    ...
-  }: let
-    inherit (pkgs) callPackage;
-    inherit (lib.extras.flakes) platformPkgs platformApps;
-    callPackageUnstable = pkgsUnstable.callPackage;
-    callPackage2311 = pkgs2311.callPackage;
-    callPackageNethermind = pkgsNethermind.callPackage;
+  perSystem =
+    {
+      self',
+      inputs',
+      pkgs,
+      pkgsUnstable,
+      pkgs2311,
+      pkgsNethermind,
+      system,
+      ...
+    }:
+    let
+      inherit (pkgs) callPackage;
+      inherit (lib.extras.flakes) platformPkgs platformApps;
+      callPackageUnstable = pkgsUnstable.callPackage;
+      callPackage2311 = pkgs2311.callPackage;
+      callPackageNethermind = pkgsNethermind.callPackage;
 
-    rustToolchain = inputs'.fenix.packages.stable.toolchain;
+      rustToolchain = inputs'.fenix.packages.stable.toolchain;
 
-    rustPlatform = pkgsUnstable.makeRustPlatform {
-      cargo = rustToolchain;
-      rustc = rustToolchain;
+      rustPlatform = pkgsUnstable.makeRustPlatform {
+        cargo = rustToolchain;
+        rustc = rustToolchain;
+      };
+    in
+    {
+      packages = platformPkgs system rec {
+        besu = callPackageUnstable ./besu { };
+        bls = callPackage ./bls { };
+        blst = callPackage ./blst { };
+        blutgang = callPackage ./blutgang { inherit (pkgsUnstable) rustPlatform; };
+        charon = callPackage ./charon { inherit bls mcl; };
+        dirk = callPackage ./dirk { inherit bls mcl; };
+        dreamboat = callPackage ./dreamboat { inherit blst; };
+        eigenlayer = callPackage ./eigenlayer { };
+        erigon = callPackage ./erigon { };
+        erigon-blst-portable = erigon.overrideAttrs (
+          _finalAttrs: _previousAttrs: {
+            CGO_CFLAGS = "-O -D__BLST_PORTABLE__";
+          }
+        );
+        eth2-testnet-genesis = callPackage ./eth2-testnet-genesis { inherit bls; };
+        eth2-val-tools = callPackage ./eth2-val-tools { inherit bls mcl; };
+        eth-validator-watcher = callPackage2311 ./eth-validator-watcher { };
+        ethdo = callPackage ./ethdo { inherit bls mcl; };
+        ethereal = callPackage ./ethereal { inherit bls mcl; };
+        evmc = callPackage ./evmc { };
+        foundry = callPackageUnstable ./foundry { inherit rustPlatform; };
+        foundry-bin = inputs.foundry-nix.defaultPackage.${system}.overrideAttrs (_oldAttrs: {
+          # TODO: Uncomment when https://github.com/shazow/foundry.nix/issues/23
+          # meta.platforms = [system];
+          meta.platforms = [
+            "x86_64-linux"
+            "aarch64-linux"
+          ];
+        });
+        geth = callPackage ./geth { };
+        geth-sealer = callPackage ./geth-sealer { };
+        heimdall = callPackage ./heimdall { };
+        lighthouse = callPackageUnstable ./lighthouse { inherit foundry; };
+        mcl = callPackage ./mcl { };
+        mev-boost = callPackage ./mev-boost { inherit blst; };
+        mev-boost-builder = callPackage ./mev-boost-builder { inherit blst; };
+        mev-boost-relay = callPackage ./mev-boost-relay { inherit blst; };
+        mev-rs = callPackage ./mev-rs { };
+        nethermind = callPackageNethermind ./nethermind { };
+        nimbus = callPackageUnstable ./nimbus { };
+        nimbus-eth2 = callPackageUnstable ./nimbus-eth2 {
+          # For now the nimbus team prefers nim 1.6 over 2.0.
+          # In newer versions of `pkgsUnstable` `nim` points to v2.0.0 in older it is 1.6.
+          # In newer versions the `nim1` package exists but not in older.
+          # See: https://github.com/status-im/nimbus-build-system/commits/master/vendor
+          nim = pkgsUnstable.nim1 or pkgsUnstable.nim;
+        };
+        prysm = callPackage ./prysm { inherit bls blst; };
+        reth = callPackageUnstable ./reth { };
+        rocketpool = callPackage ./rocketpool { };
+        rocketpoold = callPackage ./rocketpoold { inherit bls blst; };
+        rotki-bin = callPackage2311 ./rotki-bin { };
+        sedge = callPackage2311 ./sedge {
+          bls = callPackage2311 ./bls { };
+          mcl = callPackage2311 ./mcl { };
+        };
+        slither = callPackage ./slither { };
+        snarkjs = callPackage ./snarkjs { };
+        ssv-dkg = callPackage2311 ./ssv-dkg {
+          bls = callPackage2311 ./bls { };
+          mcl = callPackage2311 ./mcl { };
+        };
+        ssvnode = callPackage ./ssvnode {
+          bls = callPackage2311 ./bls { };
+          mcl = callPackage2311 ./mcl { };
+        };
+        staking-deposit-cli = callPackage ./staking-deposit-cli { };
+        teku = callPackage ./teku { };
+        tx-fuzz = callPackage ./tx-fuzz { };
+        vouch = callPackage ./vouch { inherit bls mcl; };
+        vouch-unstable = callPackage ./vouch/unstable.nix { inherit bls mcl; };
+        vscode-plugin-ackee-blockchain-solidity-tools = callPackage ./ackee-blockchain.solidity-tools { };
+        vscode-plugin-consensys-vscode-solidity-visual-editor =
+          callPackage ./consensys.vscode-solidity-auditor
+            { };
+        web3signer = callPackage ./web3signer { };
+        zcli = callPackage ./zcli { };
+      };
+
+      apps = platformApps self'.packages {
+        besu.bin = "besu";
+        blutgang.bin = "blutgang";
+        charon.bin = "charon";
+        dirk.bin = "dirk";
+        dreamboat.bin = "dreamboat";
+        erigon.bin = "erigon";
+        eth2-testnet-genesis.bin = "eth2-testnet-genesis";
+        eth2-val-tools.bin = "eth2-val-tools";
+        ethdo.bin = "ethdo";
+        ethereal.bin = "ethereal";
+        foundry = {
+          anvil.bin = "anvil";
+          cast.bin = "cast";
+          forge.bin = "forge";
+        };
+        geth = {
+          bin = "geth";
+          geth-abidump.bin = "abidump";
+          geth-abigen.bin = "abigen";
+          geth-blsync.bin = "blsync";
+          geth-bootnode.bin = "bootnode";
+          geth-clef.bin = "clef";
+          geth-devp2p.bin = "devp2p";
+          geth-ethkey.bin = "ethkey";
+          geth-evm.bin = "evm";
+          geth-faucet.bin = "faucet";
+          geth-rlpdump.bin = "rlpdump";
+        };
+        geth-sealer.bin = "geth";
+        lighthouse.bin = "lighthouse";
+        mev-boost-builder.bin = "geth";
+        mev-boost-relay.bin = "mev-boost-relay";
+        mev-boost.bin = "mev-boost";
+        mev-rs.bin = "mev";
+        nethermind = {
+          nethermind.bin = "Nethermind.Cli";
+          nethermind-runner.bin = "Nethermind.Runner";
+        };
+        nimbus = {
+          nimbus-beacon-node.bin = "nimbus_beacon_node";
+          nimbus-validator-client.bin = "nimbus_validator_client";
+        };
+        prysm = {
+          prysm-beacon-chain.bin = "beacon-chain";
+          prysm-validator.bin = "validator";
+          prysm-client-stats.bin = "client-stats";
+          prysm-ctl.bin = "prysmctl";
+        };
+        reth.bin = "reth";
+        rocketpool.bin = "rocketpool";
+        rotki-bin.bin = "rotki";
+        sedge.bin = "sedge";
+        slither.bin = "slither";
+        snarkjs.bin = "snarkjs";
+        ssv-dkg.bin = "ssv-dkg";
+        ssvnode.bin = "ssvnode";
+        staking-deposit-cli.bin = "deposit";
+        teku.bin = "teku";
+        tx-fuzz.bin = "tx-fuzz";
+        vouch.bin = "vouch";
+        zcli.bin = "zcli";
+      };
     };
-  in {
-    packages = platformPkgs system rec {
-      besu = callPackageUnstable ./besu {};
-      bls = callPackage ./bls {};
-      blst = callPackage ./blst {};
-      blutgang = callPackage ./blutgang {inherit (pkgsUnstable) rustPlatform;};
-      charon = callPackage ./charon {inherit bls mcl;};
-      dirk = callPackage ./dirk {inherit bls mcl;};
-      dreamboat = callPackage ./dreamboat {inherit blst;};
-      eigenlayer = callPackage ./eigenlayer {};
-      erigon = callPackage ./erigon {};
-      erigon-blst-portable = erigon.overrideAttrs (_finalAttrs: _previousAttrs: {
-        CGO_CFLAGS = "-O -D__BLST_PORTABLE__";
-      });
-      eth2-testnet-genesis = callPackage ./eth2-testnet-genesis {inherit bls;};
-      eth2-val-tools = callPackage ./eth2-val-tools {inherit bls mcl;};
-      eth-validator-watcher = callPackage2311 ./eth-validator-watcher {};
-      ethdo = callPackage ./ethdo {inherit bls mcl;};
-      ethereal = callPackage ./ethereal {inherit bls mcl;};
-      evmc = callPackage ./evmc {};
-      foundry = callPackageUnstable ./foundry {inherit rustPlatform;};
-      foundry-bin = inputs.foundry-nix.defaultPackage.${system}.overrideAttrs (_oldAttrs: {
-        # TODO: Uncomment when https://github.com/shazow/foundry.nix/issues/23
-        # meta.platforms = [system];
-        meta.platforms = ["x86_64-linux" "aarch64-linux"];
-      });
-      geth = callPackage ./geth {};
-      geth-sealer = callPackage ./geth-sealer {};
-      heimdall = callPackage ./heimdall {};
-      lighthouse = callPackageUnstable ./lighthouse {inherit foundry;};
-      mcl = callPackage ./mcl {};
-      mev-boost = callPackage ./mev-boost {inherit blst;};
-      mev-boost-builder = callPackage ./mev-boost-builder {inherit blst;};
-      mev-boost-relay = callPackage ./mev-boost-relay {inherit blst;};
-      mev-rs = callPackage ./mev-rs {};
-      nethermind = callPackageNethermind ./nethermind {};
-      nimbus = callPackageUnstable ./nimbus {};
-      nimbus-eth2 = callPackageUnstable ./nimbus-eth2 {
-        # For now the nimbus team prefers nim 1.6 over 2.0.
-        # In newer versions of `pkgsUnstable` `nim` points to v2.0.0 in older it is 1.6.
-        # In newer versions the `nim1` package exists but not in older.
-        # See: https://github.com/status-im/nimbus-build-system/commits/master/vendor
-        nim = pkgsUnstable.nim1 or pkgsUnstable.nim;
-      };
-      prysm = callPackage ./prysm {inherit bls blst;};
-      reth = callPackageUnstable ./reth {};
-      rocketpool = callPackage ./rocketpool {};
-      rocketpoold = callPackage ./rocketpoold {inherit bls blst;};
-      rotki-bin = callPackage2311 ./rotki-bin {};
-      sedge = callPackage2311 ./sedge {
-        bls = callPackage2311 ./bls {};
-        mcl = callPackage2311 ./mcl {};
-      };
-      slither = callPackage ./slither {};
-      snarkjs = callPackage ./snarkjs {};
-      ssv-dkg = callPackage2311 ./ssv-dkg {
-        bls = callPackage2311 ./bls {};
-        mcl = callPackage2311 ./mcl {};
-      };
-      ssvnode = callPackage ./ssvnode {
-        bls = callPackage2311 ./bls {};
-        mcl = callPackage2311 ./mcl {};
-      };
-      staking-deposit-cli = callPackage ./staking-deposit-cli {};
-      teku = callPackage ./teku {};
-      tx-fuzz = callPackage ./tx-fuzz {};
-      vouch = callPackage ./vouch {inherit bls mcl;};
-      vouch-unstable = callPackage ./vouch/unstable.nix {inherit bls mcl;};
-      vscode-plugin-ackee-blockchain-solidity-tools = callPackage ./ackee-blockchain.solidity-tools {};
-      vscode-plugin-consensys-vscode-solidity-visual-editor = callPackage ./consensys.vscode-solidity-auditor {};
-      web3signer = callPackage ./web3signer {};
-      zcli = callPackage ./zcli {};
-    };
-
-    apps = platformApps self'.packages {
-      besu.bin = "besu";
-      blutgang.bin = "blutgang";
-      charon.bin = "charon";
-      dirk.bin = "dirk";
-      dreamboat.bin = "dreamboat";
-      erigon.bin = "erigon";
-      eth2-testnet-genesis.bin = "eth2-testnet-genesis";
-      eth2-val-tools.bin = "eth2-val-tools";
-      ethdo.bin = "ethdo";
-      ethereal.bin = "ethereal";
-      foundry = {
-        anvil.bin = "anvil";
-        cast.bin = "cast";
-        forge.bin = "forge";
-      };
-      geth = {
-        bin = "geth";
-        geth-abidump.bin = "abidump";
-        geth-abigen.bin = "abigen";
-        geth-blsync.bin = "blsync";
-        geth-bootnode.bin = "bootnode";
-        geth-clef.bin = "clef";
-        geth-devp2p.bin = "devp2p";
-        geth-ethkey.bin = "ethkey";
-        geth-evm.bin = "evm";
-        geth-faucet.bin = "faucet";
-        geth-rlpdump.bin = "rlpdump";
-      };
-      geth-sealer.bin = "geth";
-      lighthouse.bin = "lighthouse";
-      mev-boost-builder.bin = "geth";
-      mev-boost-relay.bin = "mev-boost-relay";
-      mev-boost.bin = "mev-boost";
-      mev-rs.bin = "mev";
-      nethermind = {
-        nethermind.bin = "Nethermind.Cli";
-        nethermind-runner.bin = "Nethermind.Runner";
-      };
-      nimbus = {
-        nimbus-beacon-node.bin = "nimbus_beacon_node";
-        nimbus-validator-client.bin = "nimbus_validator_client";
-      };
-      prysm = {
-        prysm-beacon-chain.bin = "beacon-chain";
-        prysm-validator.bin = "validator";
-        prysm-client-stats.bin = "client-stats";
-        prysm-ctl.bin = "prysmctl";
-      };
-      reth.bin = "reth";
-      rocketpool.bin = "rocketpool";
-      rotki-bin.bin = "rotki";
-      sedge.bin = "sedge";
-      slither.bin = "slither";
-      snarkjs.bin = "snarkjs";
-      ssv-dkg.bin = "ssv-dkg";
-      ssvnode.bin = "ssvnode";
-      staking-deposit-cli.bin = "deposit";
-      teku.bin = "teku";
-      tx-fuzz.bin = "tx-fuzz";
-      vouch.bin = "vouch";
-      zcli.bin = "zcli";
-    };
-  };
 }
