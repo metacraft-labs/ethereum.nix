f02225ac6ec0b7a3ccc391d84f5a002f1e0571e3
Martin Nikov <mnikov12@gmail.com>
2025-03-26 14:45:24 +0200

    nimbus: 25.2.0 > 25.3.1

--- pkgs/nimbus/default.nix
+++ pkgs/nimbus/default.nix
@@ -4,12 +4,12 @@
   targets ? ["nimbus_beacon_node" "nimbus_validator_client" "gnosis-build" "gnosis-vc-build"],
   stableSystems ? ["x86_64-linux" "aarch64-linux"],
 }: let
-  version = "25.2.0";
+  version = "25.3.1";
   src = fetchFromGitHub {
     owner = "status-im";
     repo = "nimbus-eth2";
     rev = "v${version}";
-    hash = "sha256-INALxgkuZ4kT/DrpzMBqgNeM8Ar78BRccmGWtqoGhB0=";
+    hash = "sha256-3/FXHzq6YrJUNa9/nHaV8xXn3IOqCT5ymf6VU6wlXIk=";
     fetchSubmodules = true;
   };
 in
