9ef6c03a61a0e3a388935402e0b9cd847e35dc47
Petar Kirov <petar.p.kirov@gmail.com>
2025-08-24 00:03:49 +0300

    feat(pkgs/nimbus): Re-init at `25.7.1`

--- pkgs/by-name/ni/nimbus/default.nix
+++ pkgs/by-name/ni/nimbus/default.nix
@@ -1,16 +1,87 @@
 {
+  lib,
+  stdenv,
   fetchFromGitHub,
-  pkgs,
-  targets ? ["nimbus_beacon_node" "nimbus_validator_client" "gnosis-build" "gnosis-vc-build"],
-  stableSystems ? ["x86_64-linux" "aarch64-linux"],
-}: let
-  version = "25.4.1";
+  darwin,
+  nim,
+  cmake,
+  which,
+  writeScriptBin,
+  # Options: nimbus_light_client, nimbus_validator_client, nimbus_signing_node
+  makeTargets ? [ "all" ],
+  # These are the only platforms tested in CI and considered stable.
+  stablePlatforms ? [
+    "x86_64-linux"
+    "aarch64-linux"
+    "armv7a-linux"
+    "x86_64-darwin"
+    "aarch64-darwin"
+    "x86_64-windows"
+  ],
+}:
+stdenv.mkDerivation rec {
+  pname = "nimbus";
+  version = "25.7.1";
+  rev = "v${version}";
+
   src = fetchFromGitHub {
     owner = "status-im";
     repo = "nimbus-eth2";
-    rev = "v${version}";
-    hash = "sha256-iMfi6S+ABB/rX+NgtBTO1n8KBre7spKGf12t4dlK9gY=";
+    inherit rev;
+    hash = "sha256-oGhVKiLmBtnvCWAqMIbelU6NsWZMJk6O/ExHJFoLBqo=";
     fetchSubmodules = true;
   };
-in
-  import "${src}/nix" {inherit pkgs targets stableSystems;}
+
+  # Fix for Nim compiler calling 'git rev-parse' and 'lsb_release'.
+  nativeBuildInputs =
+    let
+      fakeGit = writeScriptBin "git" "echo $commit";
+      fakeLsbRelease = writeScriptBin "lsb_release" "echo nix";
+    in
+    [
+      fakeGit
+      fakeLsbRelease
+      nim
+      which
+      cmake
+    ]
+    ++ lib.optionals stdenv.isDarwin [ darwin.cctools ];
+
+  enableParallelBuilding = true;
+
+  # Disable CPU optmizations that make binary not portable.
+  NIMFLAGS = "-d:disableMarchNative -d:git_revision_override=${rev}";
+
+  makeFlags = makeTargets ++ [ "USE_SYSTEM_NIM=1" ];
+
+  # Generate the nimbus-build-system.paths file.
+  configurePhase = ''
+    patchShebangs scripts vendor/nimbus-build-system/scripts
+    make nimbus-build-system-paths
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    rm build/generate_makefile
+    cp build/* $out/bin
+  '';
+
+  meta = with lib; {
+    homepage = "https://nimbus.guide/";
+    downloadPage = "https://github.com/status-im/nimbus-eth2/releases";
+    changelog = "https://github.com/status-im/nimbus-eth2/blob/stable/CHANGELOG.md";
+    description = "Nimbus is a lightweight client for the Ethereum consensus layer";
+    longDescription = ''
+      Nimbus is an extremely efficient consensus layer client implementation.
+      While it's optimised for embedded systems and resource-restricted devices --
+      including Raspberry Pis, its low resource usage also makes it an excellent choice
+      for any server or desktop (where it simply takes up fewer resources).
+    '';
+    license = with licenses; [
+      asl20
+      mit
+    ];
+    mainProgram = "nimbus_beacon_node";
+    platforms = stablePlatforms;
+  };
+}
